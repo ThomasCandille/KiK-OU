9a8398a334d41f2e279f5ec32c88e7f9bf9a2893
Thomas Candille <thomascandille0508@gmail.com>
2025-12-19 09:23:37 +0100

    fix: standardize absent color usage in Location styles and adjust ProfileCard layout for consistency

--- client/src/components/ProfileCard/ProfileCard.tsx
+++ client/src/components/ProfileCard/ProfileCard.tsx
@@ -19,8 +19,6 @@ const ProfileCard = ({ imageUrl, name, role, location, mail }: ProfileCardProps)
                 <div className="profile-details">
                     <div className='profile-container'>
                         <h2 className={`profile-name ${location}`}>{name}</h2>
-                        <p className={`profile-role ${location}`}><img className={`profile-picto ${location}`} src='/profile.svg' alt='logo profile' />{role}</p>
-                        <p className={`profile-contact ${location}`}><img className={`profile-picto ${location}`} src='/mail.svg' alt='logo mail' />{mail}</p>
                     </div>
                     <div className="location">
                         <Location locationState={location} user={name === 'Silamakan TOURE' && true } />
