e523405534ef79646b424dc87f590f6027e17b18
Thomas Candille <thomascandille0508@gmail.com>
2026-03-20 11:56:14 +0100

    feat: enhance Home component with axe filtering and user display, update socket handlers for axe change events

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -6,87 +6,90 @@ import { LocationState } from '../../components/Location/Location';
 
 const socket = io(process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001');
 
+type InitialStatePayload = {
+  locations: { [key: string]: LocationState };
+  axes: string[];
+};
+
+type UsersFromAxePayload = {
+  users: string[];
+};
+
 function Home() {
+
   const [userLocationDict, setUserLocationDict] = useState<{ [key: string]: LocationState }>({});
+  const [axes, setAxes] = useState<string[]>([]);
+  const [usersFromAxe, setUsersFromAxe] = useState<string[]>([]);
+
+  const handleAxeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedAxe = event.target.value;
+    if (selectedAxe) {
+      socket.emit('axeChange', selectedAxe);
+    } else {
+      setUsersFromAxe([]);
+    }
+  };
 
   useEffect(() => {
-    socket.on('initialState', (data) => {
-      setUserLocationDict(data);
-    });
+    const handleInitialState = ({ locations, axes }: InitialStatePayload) => {
+      setUserLocationDict(locations);
+      setAxes(axes);
+    };
 
-    socket.on('statusUpdated', (data) => {
+    const handleStatusUpdated = (data: { user: string; location: LocationState }) => {
       setUserLocationDict(prev => ({
         ...prev,
         [data.user]: data.location
       }));
-    });
+    };
+
+    const handleUsersFromAxe = ({ users }: UsersFromAxePayload) => {
+      setUsersFromAxe(users);
+    };
+
+    socket.on('initialState', handleInitialState);
+    socket.on('statusUpdated', handleStatusUpdated);
+    socket.on('usersFromAxe', handleUsersFromAxe);
 
     return () => {
-      socket.off('initialState');
-      socket.off('statusUpdated');
+      socket.off('initialState', handleInitialState);
+      socket.off('statusUpdated', handleStatusUpdated);
+      socket.off('usersFromAxe', handleUsersFromAxe);
     };
   }, []);
 
   return (
     <div className="App">
-      <header> PRESENCE DES MEMBRES DE L'EQUIPE </header>
+      <header> 
+        <h1>PRESENCE DES MEMBRES DE L'EQUIPE</h1>
+        <p> - </p>
+        <select onChange={handleAxeChange}>
+          <option value="">Filtrer par axe</option>
+          {axes.map(axe => (
+            <option key={axe} value={axe}>{axe}</option>
+          ))}
+        </select>
+      </header>
       <div className='profile-card-container'>
-        <ProfileCard
-          key={'orianne-pellois'}
-          imageUrl="/icon_orianne.svg"
-          name="Orianne PELLOIS"
-          role="Responsable d'axe"
-          location={userLocationDict['orianne-pellois']}
-          mail="orianne.pellois@devinci.fr"
-          teams="Orianne Pellois"
-        />
-        <ProfileCard
-          key={'pierrick-chevron'}
-          imageUrl="/icon_pierrick.svg"
-          name="Pierrick CHEVRON"
-          role="Professeur délégué"
-          location={userLocationDict['pierrick-chevron']}
-          mail="pierrick.chevron@devinci.fr"
-          teams="Pierrick Chevron"
-        />
-        <ProfileCard
-          key={'gabriel-monier'}
-          imageUrl="/icon_gabriel.svg"
-          name="Gabriel MONIER"
-          role="Professeur délégué"
-          location={userLocationDict['gabriel-monier']}
-          mail="gabriel.monier@devinci.fr"
-          teams="Gabriel Monier"
-        />
-        <ProfileCard
-          key={'sofy-yuditskaya'}
-          imageUrl="/icon_sofy.svg"
-          name="Sofy YUDITSKAYA"
-          role="Enseignante-chercheuse"
-          location={userLocationDict['sofy-yuditskaya']}
-          mail="sofy.yuditskaya@devinci.fr"
-          teams="Sofy Yuditskaya"
-        />
-        <ProfileCard
-          key={'silamakan-toure'}
-          imageUrl="/icon_sila.svg"
-          name="Silamakan TOURE"
-          role="coordinateur pédagogique"
-          location={userLocationDict['silamakan-toure']}
-          mail="silamakan.toure@devinci.fr"
-          teams="Silamakan Toure"
-        />
-        <ProfileCard
-          key={'thomas-candille'}
-          imageUrl="/icon_thomas.svg"
-          name="Thomas CANDILLE"
-          role="Alernant"
-          location={userLocationDict['thomas-candille']}
-          mail="thomas.candille@devinci.fr"
-          teams="Thomas Candille"
-        />
-      </div>
+
+          {usersFromAxe.length > 0 ? (
+            usersFromAxe.map(user => (
+              <ProfileCard
+                key={user}
+                imageUrl={`/icon_${user.split(' ').join('-').toLowerCase()}.svg`}
+                name={user}
+                role="Membre de l'équipe"
+                location={userLocationDict[user]}
+                mail={`${user.replace(' ', '.')}@devinci.fr`}
+                teams={user}
+              />
+            ))
+          ) : (
+            <p>Aucun utilisateur trouvé pour cet axe.</p>
+          )}
+        </div>
     </div>
+        
   );
 }
 
